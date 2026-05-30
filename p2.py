1d938d43332735c0d4a907437025a81a8cfe5840
pcpcuse1-arch <pcpcuse1@gmail.com>
2026-05-30 18:38:26 +0530

    Add basic arithmetic operations to calculator app

--- p2.py
+++ p2.py
@@ -3,6 +3,17 @@ st.title("this is my first app")
 st.header("this is calculater")
 no1=st.number_input("enter the first number")
 no2=st.number_input("enter the seacond number")
-no3=no1+no2
+
 if st.button("add"):
+    no3=no1+no2
     st.write(no3)
+if st.button("subtract"):
+    no3=no1-no2
+    st.write(no3)
+if st.button("multiply"):
+    no3=no1*no2
+    st.write(no3)
+if st.button("divide"):
+    no3=no1/no2
+    st.write(no3)
+    
