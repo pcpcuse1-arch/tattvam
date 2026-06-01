b8689cf5bd0aa5a621f9b42b260554bc4c7f8251
pcpcuse1-arch <pcpcuse1@gmail.com>
2026-06-01 18:54:39 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.header("my photo gallery")
+col1,col2=st.columns(2)
+with col1:
+    st.image("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fd7913df-814c-4bc8-80e1-a8186569348f/dclgylz-7c55049a-fdfd-40d9-a391-be5833f591a5.png/v1/fill/w_1024,h_1840,q_80,strp/cristiano_ronaldo_wallpaper_lock_screen_rma_by_10mohamedmahmoud_dclgylz-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTg0MCIsInBhdGgiOiIvZi9mZDc5MTNkZi04MTRjLTRiYzgtODBlMS1hODE4NjU2OTM0OGYvZGNsZ3lsei03YzU1MDQ5YS1mZGZkLTQwZDktYTM5MS1iZTU4MzNmNTkxYTUucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.pcXDWSd61hiiQKJTP4LgtRqCrXMV0yoALsZEGRh8cnA")
+with col2:
+    st.image("https://cricketinshight.in/wp-content/uploads/2026/06/RCB-WON-CHAMPION.jpg")
+
+col3,col4=st.columns(2)
+with col3:
+    st.image("https://img-cdn.publive.online/fit-in/640x430/filters:format(webp)/odishatv/media/media_files/2026/04/13/suryavanshi-pix-2026-04-13-17-05-08.jpg")
+with col4:
+    st.image("https://i.pinimg.com/736x/40/99/9c/40999c0cafc6468757643e95f1962240.jpg")
+
+col5,col6=st.columns(2)
+with col5:
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg4jHvAhcKau9Rr9zS2HTx6q0Ygd4yNJoSNg&s")
+with col6:
+    st.image("https://i1.sndcdn.com/artworks-ryo6gV4CtqsmDJQ8-nm1jvw-t500x500.jpg")
+                
