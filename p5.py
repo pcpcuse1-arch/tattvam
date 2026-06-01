526661f3106201cce874c602a28446e49e099903
pcpcuse1-arch <pcpcuse1@gmail.com>
2026-06-01 18:10:49 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.header("🎥my youtuebe channel 🍿🎥✮⋆˙")
+col1,col2=st.columns(2)
+with col1:
+    st.video("https://youtu.be/4CMFhc8N6gk?si=toZFvuwBauAbXpqE")
+with col2:
+    st.video("https://youtu.be/-6oAZwTGD2c?si=Eg0bsuHNdeoc6B7g")
+
+
+col3,col4=st.columns(2)
+with col3:
+    st.video("https://youtu.be/xVU2UDaFOfE?si=F18kgvfNc3xRpTb_")
+with col4:
+    st.video("https://youtu.be/yALvgZi-84o?si=zmAv_z81EY6w6Sle")
+
+col5,col6=st.columns(2)
+with col5:
+    st.video("https://youtu.be/mNuhKUOD_A0?si=KSmftdBtlYc0Wajas")
+with col6:
+    st.video("https://youtu.be/2IXNyRSXQuw?si=A1F3d1cP1rOSkdBB")
